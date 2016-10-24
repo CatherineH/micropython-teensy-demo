d454b934a033138766587b8b3f132be8ac197d5b
Catherine Holloway <milankie@gmail.com>
2016-10-23 18:15:13 -0700

    adding pattern moving

--- src/main.py
+++ src/main.py
@@ -1,39 +1,71 @@
 import pyb
 
+# declare which pins go where in the PCB
+b_bank = ['D0',  'D1',  'D2',  'D3',  'D4',  'D5',  'D6',  'D7',  'D8']
+a_bank = ['D9', 'D10', 'D11', 'D12', 'D13', 'D14', 'D15', 'D16', 'D17']
 
-def pattern():
+buttons = [['D18', 'D19', 'D20'],
+           ['D21', 'D22', 'D23'],
+           ['A10', 'A11', 'A14']]
+
+pattern = [[0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+        [1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+        [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1],
+        [0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0],
+        [1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0]]
+
+directions = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+
+def move_pattern():
     # this could be a static global variable, but in anticipation of it
     # changing in future, I'm currently returning the pattern as a method
     # output.
-    return [[0, 1, 1, 1, 0, 0, 0, 0],
-            [1, 1, 0, 1, 1, 0, 0, 0],
-            [1, 0, 0, 0, 1, 1, 0, 0],
-            [1, 1, 0, 0, 0, 1, 1, 0],
-            [0, 1, 1, 0, 0, 0, 1, 1],
-            [1, 1, 0, 0, 0, 1, 1, 0],
-            [1, 0, 0, 0, 1, 1, 0, 0],
-            [1, 1, 0, 1, 1, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0, 0]]
+    global directions
+    global pattern
+    print(directions)
+    up_active = (directions[0][0] or directions[0][1] or directions[0][2])
+    down_active = (directions[2][0] or directions[2][1] or directions[2][2])
+    left_active = (directions[0][0] or directions[1][0] or directions[2][0])
+    right_active = (directions[0][2] or directions[1][2] or directions[2][2])
+    up_down = up_active - down_active
+    left_right = left_active - right_active
+    if up_down == -1:
+        # move the pattern down
+        pattern.insert(0, pattern.pop())
+    elif up_down == 1:
+        # move the pattern up
+        pattern.insert(-1, pattern.pop(0))
+    if left_right == -1:
+        for row in range(len(pattern)):
+            pattern[row].insert(0, pattern[row].pop())
+    elif left_right == 1:
+        for row in range(len(pattern)):
+            pattern[row].insert(-1, pattern[row].pop(0))
 
 
 def init_row(row_num):
+    global pattern
     # initialize an empty array of tri-state logic
-    pins = [2, 2, 2, 2, 2, 2, 2, 2, 2]
+    pins = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
     # initialize the ground pin
     pins[row_num] = 0
-    for i in range(0, 8):
-        if pattern()[row_num][i]:
+    for i in range(0, len(pins)):
+        if pattern[row_num][i]:
             # the matrix is charlieplexed. This means that if the column pin
             # is larger or equal to the row pin, it must be offset.
             pins[i if i < row_num else i + 1] = 1
     return pins
 
 
-def pin_state(i, state):
+def pin_state(pin_name, state):
     # set pin i to either low (0), high (1) or floating (2). Setting the pin
     # to floating requires setting it as an input pin (and thus has high
     # resistance)
-    pin_name = 'D' + str(i)
     if state == 2:
         pin = pyb.Pin(pin_name, pyb.Pin.IN)
     else:
@@ -45,15 +77,16 @@ def pin_state(i, state):
 
 
 def output_row(pin_row):
+    pins = a_bank + b_bank
     # set all pins in pin_row to their output state
-    for k in range(0, 9):
-        pin_state(k, pin_row[k])
+    for i in range(0, 16):
+        pin_state(pins[i], pin_row[i])
 
 
 def scroll():
     # turn each pin on, one at a time
     for j in range(0, 9):
-        for i in range(0, 8):
+        for i in range(0, 16):
             pins = [2, 2, 2, 2, 2, 2, 2, 2, 2]
             pins[j] = 0
             pins[i if i < j else i + 1] = 1
@@ -68,7 +101,17 @@ def show_pattern():
         output_row(pins)
 
 
+def read_buttons():
+    global directions
+    for i in range(0, 3):
+        for j in range(0, 3):
+            pin = pyb.Pin(buttons[i][j], pyb.Pin.IN)
+            directions[i][j] = pin.value()
+
+
 while True:
+    read_buttons()
+    move_pattern()
     # scroll()
     show_pattern()
 
